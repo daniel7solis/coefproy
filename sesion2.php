6fdcee56fcf951ba4e2f396f291e7d79289936d3
daniel7solis@gmail.com <daniel7solis@gmail.com>
2014-02-17 14:17:27 -0600

    Solucion de imagen

--- sesion2.php
+++ sesion2.php
@@ -16,8 +16,8 @@
 	<?php
 			$nombre=$_REQUEST['user'];
 			$pass=$_REQUEST['password'];
-			$conexion=mysql_connect("127.0.0.1","root","warcrack2") or die("Problemas con la conexion de base de datos ".mysql_error());
-			// $conexion=mysql_connect("127.0.0.1","root","") or die("Problemas con la conexion de base de datos ".mysql_error());
+			// $conexion=mysql_connect("127.0.0.1","root","warcrack2") or die("Problemas con la conexion de base de datos ".mysql_error());
+			$conexion=mysql_connect("127.0.0.1","root","") or die("Problemas con la conexion de base de datos ".mysql_error());
 			mysql_select_db("permisoagenda",$conexion) or die("Problemas en seleccionar la base de datos ".mysql_error());
 			mysql_set_charset("utf8", $conexion); 
 			$datos = mysql_query("select idUsuario,nombreUsuario,contrasena,semilla from usuarios where nombreUsuario='$nombre'",$conexion);
